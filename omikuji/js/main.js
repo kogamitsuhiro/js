223096214936fbfca31a5c4c290d28e9ceeb0ae7
古賀光弘 <>
2018-12-26 20:51:17 +0900

    おみくじを作成

---
+++ omikuji/js/main.js
@@ -0,0 +1,28 @@
+(function(){
+  'use strict'
+
+  var btn = document.getElementById('btn');
+
+  btn.addEventListener('click', function () {
+    var n = Math.floor(Math.random() * 3);
+    switch (n) {
+      case 0:
+        this.textContent = '大吉';
+        break;
+      case 1:
+        this.textContent = '中吉';
+        break;
+      case 2:
+        this.textContent = '凶';
+        break;
+    }
+  });
+
+  btn.addEventListener('mousedown', function () {
+    this.className = 'pushed';
+  });
+
+  btn.addEventListener('mouseup', function () {
+    this.className = '';
+  });
+})();
